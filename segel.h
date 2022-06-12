17d41e126da99d817360257194d484daeb0d12a9
nircoe <nircoe@gmail.com>
2022-06-12 10:37:39 +0000

    insertAuxToSegel

--- segel.h
+++ segel.h
@@ -23,7 +23,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <stdbool.h>
 
 
 /* Default file permissions are DEF_MODE & ~DEF_UMASK */
@@ -122,5 +122,53 @@ int open_listenfd(int portno);
 /* Wrappers for client/server helper functions */
 int Open_clientfd(char *hostname, int port);
 int Open_listenfd(int port); 
+
+typedef struct node
+{
+    int connection_fd;
+    struct node *next;
+    struct node *prev;
+    struct timeval stat_req_arrival;
+    struct timeval stat_req_dispatch;
+} * Node;
+
+typedef struct thread_info
+{
+    Node request_node;
+    pid_t thread_id;
+    int thread_index;
+    int thread_count;
+    int thread_static_count;
+    int thread_dynamic_count;
+
+    char* sched_policy;
+}*ThreadInfo;
+
+typedef struct queue
+{
+    Node head;
+    Node tail;
+    unsigned int size;
+} * Queue;
+
+Queue createQueue();
+bool pushQueue(Queue q, Node n);
+Node popQueue(Queue q);
+void displayQueue(Queue q);
+void deleteQueue(Queue q);
+void removeRandom(Queue q);
+
+typedef struct linked_list
+{
+    Node head;
+    Node tail;
+    unsigned int size;
+} * LinkedList;
+
+LinkedList createLinkedList();
+bool insertLinkedList(LinkedList ll, Node n);
+void displayLinkedList(LinkedList ll);
+bool removeFromLinkedList(LinkedList ll, Node n);
+void deleteLinkedList(LinkedList ll);
 
 #endif /* __CSAPP_H__ */
